53dd6193a399ae036aaacd71ecab26ca5ce1e474
Athir Saleem <noc7c9@gmail.com>
2021-10-28 19:55:11 +1100

    Add camera

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use macroquad::prelude::*;
 use rapier2d::prelude::*;
 
+const CAMERA_PAN_SPEED: f32 = 5.;
+const CAMERA_DEFAULT_ZOOM: f32 = 0.004;
+
 struct Context {
     entities: Vec<Entity>,
+
+    input: Input,
+
+    camera: Camera,
 
     physics_pipeline: PhysicsPipeline,
     gravity: Vector<Real>,
@@ -16,30 +23,82 @@ struct Context {
     ccd_solver: CCDSolver,
 }
 
+struct Input;
+
+impl Input {
+    fn get_wasd_axes(&self) -> Vec2 {
+        let mut delta = vec2(0., 0.);
+        if is_key_down(KeyCode::W) {
+            delta.y += 1.0;
+        }
+        if is_key_down(KeyCode::A) {
+            delta.x -= 1.0;
+        }
+        if is_key_down(KeyCode::S) {
+            delta.y -= 1.0;
+        }
+        if is_key_down(KeyCode::D) {
+            delta.x += 1.0;
+        }
+        delta.try_normalize().unwrap_or(Vec2::ZERO)
+    }
+
+    fn get_mouse_wheel(&self) -> f32 {
+        mouse_wheel().1
+    }
+}
+
+struct Camera {
+    target: Vec2,
+    zoom: f32,
+}
+
+impl Camera {
+    fn new() -> Self {
+        Self {
+            target: vec2(0., 0.),
+            zoom: CAMERA_DEFAULT_ZOOM,
+        }
+    }
+
+    fn enable(&self) {
+        set_camera(&Camera2D {
+            target: self.target,
+            zoom: vec2(self.zoom, self.zoom * screen_width() / screen_height()),
+            ..Default::default()
+        });
+    }
+
+    fn disable(&self) {
+        set_default_camera();
+    }
+}
+
 enum Entity {
     StaticRect {
-        width: f32,
-        height: f32,
+        size: Vec2,
+        color: Color,
         handle: ColliderHandle,
     },
     RigidCircle {
         radius: f32,
+        color: Color,
         handle: RigidBodyHandle,
     },
 }
 
 impl Entity {
-    fn static_rect(ctx: &mut Context, x: f32, y: f32, width: f32, height: f32) -> usize {
-        let collider = ColliderBuilder::cuboid(width / 2., height / 2.)
-            .translation(vector![x, y])
+    fn static_rect(ctx: &mut Context, position: Vec2, size: Vec2, color: Color) -> usize {
+        let collider = ColliderBuilder::cuboid(size.x / 2., size.y / 2.)
+            .translation(position.into())
             .restitution(1.)
             .friction(0.)
             .build();
         let handle = ctx.collider_set.insert(collider);
 
         let entity = Entity::StaticRect {
-            width,
-            height,
+            size,
+            color,
             handle,
         };
 
@@ -48,9 +107,9 @@ impl Entity {
         idx
     }
 
-    fn rigid_circle(ctx: &mut Context, x: f32, y: f32, radius: f32) -> usize {
+    fn rigid_circle(ctx: &mut Context, position: Vec2, radius: f32, color: Color) -> usize {
         let rigid_body = RigidBodyBuilder::new_dynamic()
-            .translation(vector![x, y])
+            .translation(position.into())
             .ccd_enabled(true)
             .build();
         let handle = ctx.rigid_body_set.insert(rigid_body);
@@ -62,7 +121,11 @@ impl Entity {
         ctx.collider_set
             .insert_with_parent(collider, handle, &mut ctx.rigid_body_set);
 
-        let entity = Entity::RigidCircle { radius, handle };
+        let entity = Entity::RigidCircle {
+            radius,
+            color,
+            handle,
+        };
 
         let idx = ctx.entities.len();
         ctx.entities.push(entity);
@@ -71,27 +134,25 @@ impl Entity {
 
     fn draw(&self, ctx: &Context) {
         use Entity::*;
-        match self {
+        match *self {
             StaticRect {
-                width,
-                height,
+                size,
+                color,
                 handle,
             } => {
-                let body = &ctx.collider_set[*handle];
-                let pos = body.translation();
-                draw_rectangle(
-                    pos.x - *width / 2.,
-                    pos.y - *height / 2.,
-                    *width,
-                    *height,
-                    BLUE,
-                );
+                let body = &ctx.collider_set[handle];
+                let pos = Vec2::from(*body.translation()) - size / 2.;
+                draw_rectangle(pos.x, pos.y, size.x, size.y, color);
             }
 
-            RigidCircle { radius, handle } => {
-                let body = &ctx.rigid_body_set[*handle];
-                let pos = body.translation();
-                draw_circle(pos.x, pos.y, *radius, YELLOW);
+            RigidCircle {
+                radius,
+                color,
+                handle,
+            } => {
+                let body = &ctx.rigid_body_set[handle];
+                let pos = *body.translation();
+                draw_circle(pos.x, pos.y, radius, color);
             }
         }
     }
@@ -109,6 +170,10 @@ async fn main() {
     let mut ctx = Context {
         entities: vec![],
 
+        input: Input,
+
+        camera: Camera::new(),
+
         physics_pipeline: PhysicsPipeline::new(),
         gravity: vector![0., 0.],
         integration_parameters: IntegrationParameters::default(),
@@ -121,21 +186,24 @@ async fn main() {
         ccd_solver: CCDSolver::new(),
     };
 
+    let screen_size = vec2(screen_width(), screen_height());
+    let screen_center = screen_size / 2.;
+
     // Create the boundaries
-    let w = screen_width();
-    let h = screen_height();
-    let cx = w / 2.;
-    let cy = h / 2.;
-    Entity::static_rect(&mut ctx, cx, 0., w, 10.);
-    Entity::static_rect(&mut ctx, cx, h, w, 10.);
-    Entity::static_rect(&mut ctx, 0., cy, 10., h);
-    Entity::static_rect(&mut ctx, w, cy, 10., h);
+    {
+        let s = screen_size;
+        let c = screen_center;
+        Entity::static_rect(&mut ctx, vec2(0., -c.y), vec2(s.x + 10., 10.), BLUE);
+        Entity::static_rect(&mut ctx, vec2(0., c.y), vec2(s.x + 10., 10.), BLUE);
+        Entity::static_rect(&mut ctx, vec2(-c.x, 0.), vec2(10., s.y + 10.), BLUE);
+        Entity::static_rect(&mut ctx, vec2(c.x, 0.), vec2(10., s.y + 10.), BLUE);
+    }
 
     // Create ball
     for _ in 0..100 {
-        let x = rand::gen_range(30., screen_width() - 30.);
-        let y = rand::gen_range(30., screen_height() - 30.);
-        let idx = Entity::rigid_circle(&mut ctx, x, y, 5.);
+        let x = rand::gen_range(-screen_center.x + 25., screen_center.x - 25.);
+        let y = rand::gen_range(-screen_center.y + 25., screen_center.y - 25.);
+        let idx = Entity::rigid_circle(&mut ctx, vec2(x, y), 5., YELLOW);
 
         let dx = rand::gen_range(-500., 500.);
         let dy = rand::gen_range(-500., 500.);
@@ -159,11 +227,19 @@ async fn main() {
             &(),
         );
 
-        clear_background(BLACK);
+        // read input
+        ctx.camera.target += ctx.input.get_wasd_axes() * CAMERA_PAN_SPEED;
+        ctx.camera.zoom *= 1.1f32.powf(ctx.input.get_mouse_wheel());
 
+        ctx.camera.enable();
+
+        // draw
+        clear_background(BLACK);
         for entity in &ctx.entities {
             entity.draw(&ctx);
         }
+
+        ctx.camera.disable();
 
         next_frame().await
     }
