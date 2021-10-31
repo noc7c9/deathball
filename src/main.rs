be5221c65c10181ed282a3875e881a720bc6d2d0
Athir Saleem <noc7c9@gmail.com>
2021-10-31 13:23:59 +1100

    Refactor input into its own module

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use macroquad::prelude::*;
 
 mod physics;
 use physics::Physics;
+mod input;
+use input::Input;
 
 const CAMERA_PAN_SPEED: f32 = 15.;
 const CAMERA_DEFAULT_ZOOM: f32 = 0.001;
@@ -13,75 +15,6 @@ struct Context {
     input: Input,
     camera: Camera,
     physics: Physics,
-}
-
-struct Input {
-    drag: Option<MouseDrag>,
-}
-
-struct MouseDrag {
-    previous: Vec2,
-    current: Vec2,
-}
-
-impl Input {
-    fn new() -> Self {
-        Input { drag: None }
-    }
-
-    fn update(&mut self) {
-        let is_down = is_mouse_button_down(MouseButton::Right);
-        match self.drag {
-            // start drag
-            None if is_down => {
-                let position = mouse_position().into();
-                self.drag = Some(MouseDrag {
-                    previous: position,
-                    current: position,
-                });
-            }
-            // continue drag
-            Some(ref mut drag) if is_down => {
-                drag.previous = drag.current;
-                drag.current = mouse_position().into();
-            }
-            // end drag
-            Some(_) if !is_down => self.drag = None,
-            _ => {}
-        }
-    }
-
-    fn get_mouse_drag(&self) -> Option<&MouseDrag> {
-        self.drag.as_ref()
-    }
-
-    fn get_wasd_axes(&self) -> Vec2 {
-        let mut delta = vec2(0., 0.);
-        if is_key_down(KeyCode::W) {
-            delta.y += 1.0;
-        }
-        if is_key_down(KeyCode::A) {
-            delta.x -= 1.0;
-        }
-        if is_key_down(KeyCode::S) {
-            delta.y -= 1.0;
-        }
-        if is_key_down(KeyCode::D) {
-            delta.x += 1.0;
-        }
-        delta.try_normalize().unwrap_or(Vec2::ZERO)
-    }
-
-    fn get_mouse_wheel(&self) -> Option<f32> {
-        let value = mouse_wheel().1;
-        if value == 0.0 {
-            None
-        } else if cfg!(target_arch = "wasm32") {
-            Some(value.clamp(-1., 1.))
-        } else {
-            Some(value.clamp(-2., 2.))
-        }
-    }
 }
 
 struct Camera {
