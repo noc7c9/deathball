0a8cfcda213350de4fa5564abf40af7f7d25b3fb
noc7c9 <noc7c9@gmail.com>
2021-12-10 23:44:46 +0000

    Deploying to gh-pages from @ noc7c9/deathball@8fb2270aa5daa1b8c052b30586e0d81c35629a1f 🚀

--- deathball.js
+++ deathball.js
@@ -470,7 +470,7 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper1305 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper1299 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 61, __wbg_adapter_26);
         return addHeapObject(ret);
     };
